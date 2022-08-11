61fccaeb88586a3b8715aae45e08c68ac8f17ee4
Philimonnag <philimonnag@gmail.com>
2022-08-12 00:41:43 +0530

    Thank you Jesus for helping me to write code

--- app/src/main/java/com/philimonnag/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/philimonnag/tipcalculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.philimonnag.tipcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.philimonnag.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.jvm.internal.Intrinsics
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -10,5 +12,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField=binding.plainTextInput.text.toString()
+        val cost =stringInTextField.toDouble()
+        val selectedId=binding.tipOption.checkedRadioButtonId
+        val tipPercentage=when(selectedId){
+            R.id.option_twenty_percent->0.20
+            R.id.option_eighteen_percent->0.18
+            else->0.15
+        }
+        var tip=tipPercentage*cost
+        val roundUp=binding.roundupSwitch.isChecked
+        if(roundUp){
+            tip=kotlin.math.ceil(tip)
+        }
+        val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text=getString(R.string.tip_amount,formattedTip)
+
     }
 }
